476a91afe1f69f4abd839028ce86d1e27cfa3524
JovianaYoung <jovianayoung8376@gmail.com>
2024-04-04 22:32:07 +0800

    web3jovi

--- joviweb3.sql
+++ joviweb3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 04, 2024 at 08:16 AM
+-- Generation Time: Apr 04, 2024 at 04:25 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `miniproweb3`
+-- Database: `joviweb3`
 --
 
 -- --------------------------------------------------------
