0977de2bac5c3bf7b732c873f09a96c3e3ef666d
JovianaYoung <124419335+JovianaYoung@users.noreply.github.com>
2024-04-05 21:36:30 +0800

    wepro3

--- joviweb3.sql
+++ joviweb3.sql
@@ -93,7 +93,7 @@ INSERT INTO `products` (`product_id`, `p_category_id`, `p_material_id`, `product
 (35, 1, 3, 'Meja Logam', 'Tinggikan rumah Anda dengan dekorasi yang memadukan fungsionalitas dan daya tarik estetika.', 95.00, 0, 'mejalogam3a.WEBP', 'mejalogam3aa.WEBP', 'mejalogam3aaa.WEBP'),
 (39, 4, 5, 'Tempat Tidur Kulit', 'Rasakan kenyamanan tak tertandingi dan malam yang tenang dengan koleksi tempat tidur kami yang luar biasa.', 300.00, 0, 'kasurkulit2a.WEBP', 'kasurkulit2aa.WEBP', 'kasurkulit2aaa.WEBP'),
 (40, 1, 3, 'Meja Logam', 'Tinggikan rumah Anda dengan dekorasi yang memadukan fungsionalitas dan daya tarik estetika.', 130.00, 0, 'jovimeja1.WEBP', 'jovimeja2.WEBP', 'jovimeja3.WEBP'),
-(53, 7, 3, 'Kursi Logam', 'Dibuat dengan cermat dengan memperhatikan detail, kursi kami menawarkan perpaduan sempurna antara fungsionalitas dan keanggunan.', 65.00, 25, 'chair5a-metal.jpg', 'chair5b-metal.jpg', 'chair5c-metal.jpg');
+(53, 7, 3, 'Kursi Logam', 'Dibuat dengan cermat dengan memperhatikan detail, kursi kami menawarkan perpaduan sempurna antara fungsionalitas dan keanggunan.', 65.00, 25, 'kursilogam41.WEBP', 'kursilogam42.WEBP', 'kursilogam43.WEBP');
 
 -- --------------------------------------------------------
 
